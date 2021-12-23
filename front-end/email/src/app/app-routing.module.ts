26016825bec1af629e329ae0793d9d6c296c1e4c
aaserfawzy <aaserkind537@gmail.com>
2021-12-23 23:55:13 +0200

    el route

--- front-end/email/src/app/app-routing.module.ts
+++ front-end/email/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'main/:id', component:MainComponent  },
+
 ];
 
 @NgModule({
--- front-end/email/src/app/main/main.component.ts
+++ front-end/email/src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-main',
@@ -17,41 +18,54 @@ export class MainComponent implements OnInit {
   viewD:Boolean=false;
   viewT:Boolean=false;
   filter?:string;
+  routnig?:string;
+  id?:string;
 
-  constructor() { }
+  constructor(private route:ActivatedRoute,
+              private router:Router) {
+
+              }
 
   ngOnInit(): void {
+    let id=this.route.snapshot.paramMap.get('id');
+    console.log(id);
   }
+  activet(a:number){
+    console.log(a)
 
-  active(a:number){
+    this.router.navigate(['/main',a]);
+  }
+  active(a:string){
+    console.log(a)
     this.view=true
     this.profile=false;
     this.search=false;
     this.make=false;
-    if(a==1){
+    if(a=='1'){
       this.viewI=true;
-
       this.viewS=false;
       this.viewD=false;
       this.viewT=false;
     }
-    else if(a==2){
+    else if(a=='2'){
       this.viewS=true;
       this.viewI=false;
       this.viewD=false;
       this.viewT=false;
     }
-    else if(a==3){
+    else if(a=='3'){
       this.viewD=true;
       this.viewI=false;
       this.viewS=false;
       this.viewT=false;
     }
-    else if(a==4){
+    else if(a=='4'){
       this.viewT=true;
       this.viewI=false;
       this.viewS=false;
       this.viewD=false;
+    }else{
+      console.log("a7a")
     }
 
 
