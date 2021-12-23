2d9f4cc1870b96b2dd6c12fbeff162fc4002f096
aaserfawzy <aaserkind537@gmail.com>
2021-12-23 11:35:47 +0200

    ui start

--- back-end/email_server/src/main/java/com/emailserver/email_server/userAndMessage/contact.java
+++ back-end/email_server/src/main/java/com/emailserver/email_server/userAndMessage/contact.java
@@ -1,5 +1,5 @@
 package com.emailserver.email_server.userAndMessage;
-
+/* read only interface */
 public interface contact {
     public int getID();
     public String getUserName();
--- front-end/email/src/app/app.module.ts
+++ front-end/email/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatSliderModule } from '@angular/material/slider';
+import {MatSidenavModule} from '@angular/material/sidenav';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
+
     AppComponent
   ],
   imports: [
+    MatSidenavModule,
+    MatSliderModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
