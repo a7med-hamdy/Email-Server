0c08a3d512f5b3b8f36a2111e4534836d453080c
Ahmed-Ashour-2024 <mody.ashour.726@gmail.com>
2021-12-26 07:46:00 +0200

    maker ui done but feeh problem in request

--- front-end/email/src/app/maker/maker.component.ts
+++ front-end/email/src/app/maker/maker.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit,Inject ,ViewChild } from '@angular/core';
-import { HttpEventType, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpEventType, HttpParams, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { FileUploadService } from 'src/app/services/file-upload.service';
 import { urlx } from './type'
 import { DomSanitizer, SafeResourceUrl, SafeUrl} from '@angular/platform-browser';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 
 
@@ -22,17 +24,81 @@ export class MakerComponent implements OnInit {
 
   selectedFiles?: FileList;
 
+  url = "http://localhost:8080";
 
-  constructor(private uploadService: FileUploadService,private sanitizer: DomSanitizer) { }
+  constructor(private uploadService: FileUploadService,private sanitizer: DomSanitizer,
+              private http: HttpClient,
+              private fb: FormBuilder,
+              private router: Router) { }
 
   fileList: File[] = [];
   urls:SafeUrl[] = [];
 
   ngOnInit(): void {
   }
+  /******************************************************* */
+  messageForm = this.fb.group({
+    // ID: [''],
+    toEmails: this.fb.array([
+      this.fb.control('', [Validators.email, Validators.minLength(2)])
+    ]),
+    subject: [''],
+    body: [''],
+    priority: [''],
+    /* attatchments: this.fb.array([
+      this.fb.control('')
+    ]), */
+  });
 
+  // make message (sent or draft) - request
+  makeMessage(Type: string){
+    let _url = `${this.url}/makeMessage`;
+    let bdy = this.messageForm.value.body;
+    let subj: string= this.messageForm.value.subject;
+    let pri: number = this.messageForm.value.priority;
+    let receivers_emails: string[] = this.messageForm.value.toEmails;
+    // let attachs: string[] = this.messageForm.value.attachements;
+    console.log(receivers_emails, subj)
+    /* let params = new HttpParams()
+    params = params.append('subject',subj)
+    params = params.append('body',bdy)
+    params = params.append('type',Type)
+    params = params.append('priority',pri) */
+    return this.http.post<any>(_url,
+      {params: {/* recs: receivers_emails, */ subject: subj, body: bdy, 
+                type: Type, priority: pri}})
+    .subscribe(done => {
+      if(done){
+        console.log("Message composed & saved successfully!!");
+      }
+      else{
+        console.log("Error!! Something went WRONG!!");
+      }
+    }/* ,err => {alert("something went WRONG!!")} */)
+  }
 
-
+  makeMessageOfType(type: string){
+    console.log(this.messageForm.value)
+    this.makeMessage(type)
+  }
+  
+  get toEmails(){
+    return this.messageForm.get('toEmails') as FormArray;
+  }
+  addTo(){
+    if(this.toEmails.length !== 5){
+      this.toEmails.push(this.fb.control('', Validators.email));
+    }
+  }
+  deleteTo(i: number){
+    if(this.toEmails.length !== 1){
+      this.toEmails.removeAt(i);
+    }
+  }
+  enableSentBtn(): boolean{
+    return this.messageForm.valid  && (this.messageForm.value.toEmails[0] as string).length !== 0;
+  }
+  /* ****************************************************** */
 
   selectFiles(event:any): void {
     this.urls = [];
