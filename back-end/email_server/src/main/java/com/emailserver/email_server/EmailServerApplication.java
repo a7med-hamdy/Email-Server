5aab0c9827f56d8ac13772ed71894c96e2848ec4
a7med-hamdy <hamdiahmed906@gmail.com>
2021-12-27 11:54:58 +0200

    dummy

--- back-end/email_server/src/main/java/com/emailserver/email_server/EmailServerApplication.java
+++ back-end/email_server/src/main/java/com/emailserver/email_server/EmailServerApplication.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import com.emailserver.email_server.Server.Server;
 import com.emailserver.email_server.userAndMessage.message;
@@ -14,7 +15,6 @@ import com.emailserver.email_server.userAndMessage.userContact;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.multipart.MultipartFile;
 
 @SpringBootApplication
 public class EmailServerApplication {
@@ -22,7 +22,6 @@ public class EmailServerApplication {
 	public static void main(String[] args) throws IOException{
 		
 		SpringApplication.run(EmailServerApplication.class, args);
-		/*
 		Server s = Server.getInstanceOf();
 		Server s2 = Server.getInstanceOf();
 		ArrayList<userContact> contacts = new ArrayList<>();
@@ -58,27 +57,30 @@ public class EmailServerApplication {
 		messageMaker maker = new messageMaker();
 		////////
 		Date now=new Date();
-		message n = maker.getNewMessage(1111, "hello", 5,342544, to, "subject", now, 1, new ArrayList<String>());
-		message n1 = maker.getNewMessage(2222, "hello", 5,342544, to1, "subject", now, 1, new ArrayList<String>());
-		message n2 = maker.getNewMessage(3333, "hello", 5,342544, to2, "subject", now, 1, new ArrayList<String>());
-		message n3 = maker.getNewMessage(4444, "hello", 5,342544, to3, "subject", now, 1, new ArrayList<String>());
-		message n4 = maker.getNewMessage(5555, "hello", 5,342544, to4, "subject", now, 1, new ArrayList<String>());
-		message n5 = maker.getNewMessage(6666, "hello", 5,342544, to5, "subject", now, 1, new ArrayList<String>());
-		message n6 = maker.getNewMessage(7777, "hello", 5,342544, to6, "subject", now, 1, new ArrayList<String>());
-		message x = maker.getNewMessage(1010, "another message", 15,342544, to2, "TWO", now, 4, new ArrayList<String>());
+		message n = maker.getNewMessage(1111, "hello", 5,342544, to, "subject", now, 1, new ArrayList<>(Arrays.asList("image.jpf")));
+		message n1 = maker.getNewMessage(2222, "hello", 5,342544, to1, "subject", now, 1, new ArrayList<>(Arrays.asList("image.jpf")));
+		message n2 = maker.getNewMessage(3333, "hello", 5,342544, to2, "subject", now, 1, new ArrayList<>(Arrays.asList("image.jpf")));
+		message n3 = maker.getNewMessage(4444, "hello", 5,342544, to3, "subject", now, 1, new ArrayList<>(Arrays.asList("image.jpf")));
+		message n4 = maker.getNewMessage(5555, "hello", 5,342544, to4, "subject", now, 1, new ArrayList<>(Arrays.asList("image.jpf")));
+		message n5 = maker.getNewMessage(6666, "hello", 5,342544, to5, "subject", now, 1, new ArrayList<>(Arrays.asList("image.jpf")));
+		message n6 = maker.getNewMessage(7777, "hello", 5,342544, to6, "subject", now, 1, new ArrayList<>(Arrays.asList("image.jpf")));
+		message x = maker.getNewMessage(1010, "another message", 15,342544, to2, "TWO", now, 4, new ArrayList<>((Arrays.asList("image.jpf"))));
+		
 		//////////
-		/*s.senew ArrayList<String>();*/
+		s.sendMessage(n);
+		s.sendMessage(n1);
+		s.sendMessage(n2);
 		s.sendMessage(n3);
 		s.sendMessage(n4);
 		s.sendMessage(n5);
 		s.sendMessage(n6);
 		// s.addAttachment(1111);
-		System.out.println(s.requestFolder(342544, "sent","time",1).toString());
-		s.createFolder(555, "tobe");
+		// System.out.println(s.requestFolder(342544, "sent","time",1).toString());
+		// s.createFolder(555, "tobe");
 		// s.moveMessage(555, 10, "inbox","tobe");
 		// s.createFolder(234, "mails");
 		// s.moveMessage(234, 10, "inbox","mails");
-		// s.renameFolder(234, "newName", "mails");*/
+		// s.renameFolder(234, "newName", "mails");
 	}
 
 }
