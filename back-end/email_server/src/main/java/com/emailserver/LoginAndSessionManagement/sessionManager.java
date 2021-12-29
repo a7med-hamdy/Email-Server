c69aaac538d56e5e8023cb57153f2b833a779f0f
mohamedezzat2000z <mohamedezzat2000z@gmail.com>
2021-12-29 16:24:41 +0200

    limit in session pool

--- back-end/email_server/src/main/java/com/emailserver/LoginAndSessionManagement/sessionManager.java
+++ back-end/email_server/src/main/java/com/emailserver/LoginAndSessionManagement/sessionManager.java
@@ -25,16 +25,19 @@ public class sessionManager {
      * @param password user's password
      * @param userEmail user's email
      */
-    public void createSession(int userID, String userName, String password,String userEmail){
-        sessionInterface session = new session();
-        int min=1,max=1000000000;
-        int newID=(int)Math.floor(Math.random()*(max-min+1)+min);         
-        session.setSessionID((newID));
-        session.setUserId((int)userID);
-        session.setUserName(userName);
-        session.setUserPassword(password);
-        session.setUserEmail(userEmail);
-        this.sessions.add(session);    
+    public void createSession(int userID, String userName, String password,String userEmail)throws ArrayIndexOutOfBoundsException{
+        if(this.getSessions().size() <=1000){
+            sessionInterface session = new session();
+            int min=1,max=1000000000;
+            int newID=(int)Math.floor(Math.random()*(max-min+1)+min);         
+            session.setSessionID((newID));
+            session.setUserId((int)userID);
+            session.setUserName(userName);
+            session.setUserPassword(password);
+            session.setUserEmail(userEmail);
+            this.sessions.add(session);
+        }
+        else {throw new ArrayIndexOutOfBoundsException();}
     }
 
     /**
